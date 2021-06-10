86828844ee4adc4e59b703ee98f7cfde712eb421
0x0a0d <vandug23@gmail.com>
2021-06-10 12:08:36 +0700

    add mixin-errors

---
+++ packages/moleculer-sf-mixin-errors/index.js
@@ -0,0 +1,33 @@
+const Errors = require('moleculer').Errors
+
+function extendClass(name, className) {
+  return {
+    [name]: class extends className {}
+  }[name]
+}
+
+const $errors = new Proxy(Errors, {
+  get(target, p) {
+    if (typeof p !== 'string' || Object.prototype.hasOwnProperty.call(target, p)) {
+      return target[p]
+    }
+    let classTarget = 'MoleculerError'
+    if (p.endsWith('ClientError')) {
+      classTarget = 'MoleculerClientError'
+    }
+    if (p.endsWith('RetryableError')) {
+      classTarget = 'MoleculerRetryableError'
+    }
+    if (p.endsWith('ServerError')) {
+      classTarget = 'MoleculerServerError'
+    }
+    return extendClass(p, target[classTarget])
+  }
+})
+
+module.exports = {
+  $errors: $errors,
+  created() {
+    this.$errors = $errors
+  }
+}
