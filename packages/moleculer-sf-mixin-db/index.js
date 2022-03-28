cdd4b20d25f31ceb9a7a6d16385e3989f201d274
0x0a0d <vandug23@gmail.com>
2022-03-29 06:34:39 +0700

    change moleculer-db to @cylution/moleculer-db

--- packages/moleculer-sf-mixin-db/index.js
+++ packages/moleculer-sf-mixin-db/index.js
@@ -1,5 +1,5 @@
 const pluralize = require('mongoose').Mongoose.prototype.pluralize()
-const DbService = require('moleculer-db')
+const DbService = require('@cylution/moleculer-db')
 const MongoAdapter = require('@cylution/moleculer-db-adapter-mongoose')
 const mongoose = require('mongoose')
 
