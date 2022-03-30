0ae444c068a1fd7ea241f14572a01a8351b4e0ce
0x0a0d <vandug23@gmail.com>
2022-03-30 14:40:56 +0700

    remove unused

--- packages/moleculer-sf-prevent-change-created-at/README.md
+++
@@ -1,3 +0,0 @@
-# Deprecated!
-
-Use [@cylution/mongoose-plugin-timestamps-immutable](https://www.npmjs.com/package/@cylution/mongoose-plugin-timestamps-immutable) instead
--- packages/moleculer-sf-prevent-change-created-at/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(serviceSchema) {
-  if (serviceSchema.schema != null) {
-    serviceSchema.schema.plugin(function(dbSchema) {
-      if (dbSchema.options.timestamps) {
-        dbSchema.path(dbSchema.$timestamps.createdAt).immutable(true)
-      }
-    })
-  }
-}
